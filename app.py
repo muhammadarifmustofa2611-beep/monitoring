f56ddf7221bd18769e86d62c1a8e51a73fb9d55e
muhammadarifmustofa2611-beep <muhammadarifmustofa2611@gmail.com>
2026-09-20 14:00:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def show_welcome_popup():
             <b>Hasil Eskalasi</b> sudah sesuai.
         </p>
         <p style="margin-bottom: 0;">
-            <b>Jangan Banyak Banyak ya, Syukron Yale Yale</b>
+            <b>Jangan Banyak Banyak ya, Syukron Yale Yale🤣</b>
         </p>
     </div>
     """, unsafe_allow_html=True)
